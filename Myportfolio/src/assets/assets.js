1369d68fcb96be571584ba288ec00c19f2d9f28e
vivek <vivekrandhawa6@gmail.com>
2025-07-18 19:14:32 +0530

    update portfolio

--- Myportfolio/src/assets/assets.js
+++ Myportfolio/src/assets/assets.js
@@ -1,7 +1,11 @@
 import project1 from "./project.png";
 import project2 from "./project2.png";
 import project3 from "./project3.png";
+import project4 from "./demo1.png";
+import project5 from "./project5.png"
 
+import html_logo from "./html.png";
+import css_logo from "./css.png";
 import React_logo from "./React-icon.png";
 import tailwind_logo from "./tailwindlogo.png";
 import boostrap_logo from "./boostraplogo.png";
@@ -15,6 +19,9 @@ import sql_logo from "./sql.png";
 import git_logo from "./git.png";
 import github_logo from "./github.png";
 import rest_logo from "./rest.png";
+import docker_logo from "./docker.png";
+import redux_logo from "./redux.png";
+import socket_logo from "./socket.png" 
 
 import python_logo from "./python_logo.png";
 import cpp_logo from "./cpp_logo.png";
@@ -24,6 +31,20 @@ import javascript_logo from "./javascript_logo.png";
 import bgv from "./bgv.mp4";
 
 let projects = [
+    {
+    image: project5,
+    title: "Lectora - Ai",
+    description:
+      "Lectora-Ai is a Ai-powered lecture builder.You can build lectures with just a single prompt .",
+    link: "https://lectora-ai.onrender.com",
+  },
+    {
+    image: project4,
+    title: "AI-Think",
+    description:
+      "AI-Think is a web application that helps user to find new and trending Idea for their project. It uses github,twitter and reddit to generate ideas based on user input.",
+    link: "https://ai-think-idea-aggregator.vercel.app/",
+  },
   {
     image: project1,
     title: "QuickCuisine",
@@ -37,7 +58,6 @@ let projects = [
     description:
       "SmartBook is a MERN stack-based app offering seemless appointment booking services. For both small businesses as well as customers.",
     link: "https://smart-appointment-booking-system-client-ke7j.onrender.com",
-
   },
   {
     image: project3,
@@ -45,17 +65,18 @@ let projects = [
     description:
       "MemeStream is a real-time chat app using EJS, Node.js, MongoDB, and Cloudinary for seamless communication and media sharing.",
     link: "https://memefy-zx5p.onrender.com",
-
   },
+
+
 ];
 
-let frontendlogo = [React_logo, tailwind_logo, boostrap_logo];
+let frontendlogo = [html_logo,css_logo , React_logo, tailwind_logo, boostrap_logo];
 
-let backendlogo = [express_logo, node_logo];
+let backendlogo = [express_logo, node_logo ,rest_logo];
 
 let database = [mongo_logo, sql_logo];
 
-let othertechnology = [git_logo, github_logo, rest_logo];
+let othertechnology = [git_logo, github_logo ,docker_logo,redux_logo,socket_logo];
 
 let programminglanguage = [python_logo, cpp_logo, c_logo, javascript_logo];
 const assets = {
@@ -65,8 +86,7 @@ const assets = {
   database,
   othertechnology,
   programminglanguage,
-  bgv
+  bgv,
 };
 
 export default assets;
-
